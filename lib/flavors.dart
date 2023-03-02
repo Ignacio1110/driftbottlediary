7fb28a8ea9f4e794703eb3598a58c943cdfac7e0
Ignacio <qq1179988568@gmail.com>
2023-03-03 01:03:07 +0800

    feat use flavor

--- lib/flavors.dart
+++ lib/flavors.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'shared_module/admob_service.dart';
+
 enum Flavor {
   ENV_PROD,
   ENV_DEV,
@@ -19,4 +23,38 @@ class F {
     }
   }
 
+  static String get bannerId {
+    switch (appFlavor) {
+      case Flavor.ENV_PROD:
+        return Platform.isAndroid
+            ? 'ca-app-pub-your------ id'
+            : 'ca-app-pub-your------- id';
+      default:
+        return Platform.isAndroid
+            ? 'ca-app-pub-3940256099942544/6300978111'
+            : 'ca-app-pub-3940256099942544/2934735716';
+    }
+  }
+
+  static String get interstitialId {
+    switch (appFlavor) {
+      case Flavor.ENV_PROD:
+        return 'paste your id';
+      default:
+        return Platform.isAndroid
+            ? 'ca-app-pub-3940256099942544/1033173712'
+            : 'ca-app-pub-3940256099942544/4411468910';
+    }
+  }
+
+  static String get rewardedId {
+    switch (appFlavor) {
+      case Flavor.ENV_PROD:
+        return 'paste your id';
+      default:
+        return Platform.isAndroid
+            ? 'ca-app-pub-3940256099942544/5224354917'
+            : 'ca-app-pub-3940256099942544/1712485313';
+    }
+  }
 }
