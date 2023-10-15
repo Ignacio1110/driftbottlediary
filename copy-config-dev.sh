469768e7b1c33f67f31cbff7a0f27fb531d05d5e
Ignacio <qq1179988568@gmail.com>
2023-10-15 17:32:06 +0800

    fix: firebase file error

--- copy-config-dev.sh
+++ copy-config-dev.sh
@@ -1,3 +1,4 @@
 cp config/GoogleService-Info-dev.plist ios/config/env_dev/GoogleService-Info.plist
+cp config/GoogleService-Info-dev.plist ios/Runner/GoogleService-Info.plist
 cp config/google-services-dev.json android/app/google-services.json
 cp config/firebase_app_id_file-dev.json ios/firebase_app_id_file.json
--- copy-config-prod.sh
+++ copy-config-prod.sh
@@ -1,3 +1,4 @@
 cp config/GoogleService-Info-prod.plist ios/config/env_prod/GoogleService-Info.plist
+cp config/GoogleService-Info-prod.plist ios/Runner/GoogleService-Info.plist
 cp config/google-services-prod.json android/app/google-services.json
 cp config/firebase_app_id_file-prod.json ios/firebase_app_id_file.json
